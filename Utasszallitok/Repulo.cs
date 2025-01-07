f2d3717834b551f2b63e553897900641f3c2526f
Bo1129 <bohmb.22d@acsjszki.hu>
2025-01-07 12:57:28 +0100

    3

---
+++ Utasszallitok/Repulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utasszallitok
+{
+    internal class Repulo
+    {
+        public string tipus { get; set; }
+        public string ev { get; set; }
+        public string utas { get; set; }
+        public int szemelyzet { get; set; }
+        public int utazosebesseg { get; set; }
+        public int felszallotomeg { get; set; }
+        public int fesztav { get; set; }
+        public Repulo(string sor)
+        {
+            string[] s = sor.Split(';');
+            tipus = s[0];
+            ev = s[1];
+            utas = s[2];
+            szemelyzet = int.Parse(s[3]);
+            utazosebesseg = int.Parse(s[4]);
+            felszallotomeg = int.Parse(s[5]);
+            fesztav = int.Parse(s[6]);
+        }
+    }
+}
