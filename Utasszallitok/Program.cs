5c1157c9545defa03b418adf620a22db5719c472
Bo1129 <bohmb.22d@acsjszki.hu>
2025-01-07 13:19:36 +0100

    5

--- Utasszallitok/Program.cs
+++ Utasszallitok/Program.cs
@@ -9,8 +9,21 @@
             {
                 repulok.Add(new Repulo(sor));
             }
+
             //4. feladat
             Console.WriteLine($"4. feladat: Adatsorok száma: {repulok.Count}");
+
+            //5.feladat
+            int rep = 0;
+            foreach (var r in repulok)
+            {
+
+                if (r.tipus == "Boeing")
+                {
+                    rep++;
+                }
+            }
+            Console.WriteLine($"5.feladat: Boeing tipusok száma: {rep}");
         }
     }
 }
